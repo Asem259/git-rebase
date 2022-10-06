545fb72bbecc58956775fed1236b3e89b04d569d
asem259 <alghabra.asem@gmail.com>
2022-10-06 15:48:50 +0200

    add two strings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Dennis war hier");
@@ -11,11 +13,19 @@ System.out.println("Hello vom asem branch!");
         System.out.println("Hello vom asem branch!");
         System.out.println("Dennis war hier!");
         System.out.println("Mit seinem 1 side Branch vom Masterbranch!!!");
+        System.out.println(addStrings("Git", "rebase"));
 
         System.out.println("Hello vom asem branch!");
 
         int result = addValue(1,2);
         System.out.println("Result : " + result);
+    }
+
+
+    public static String addStrings(String str1, String str2) {
+        return str1 + " " + str2;
+
+
     }
 
     public static int addValue(int value, int value2){
