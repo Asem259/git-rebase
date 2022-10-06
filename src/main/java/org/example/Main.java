c320e6237d0a82fb99b6e11324e40bd45d65f104
voelkldennis <voelkldennis@googlemail.com>
2022-10-06 14:51:01 +0200

    add Dennis 1. Branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Dennis war hier");
+        System.out.println("Mit seinem 1 side Branch vom Masterbranch");
+
     }
 }
