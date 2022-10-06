b4070c05d60ac7188d339fe5536c7b18d0121386
voelkldennis <voelkldennis@googlemail.com>
2022-10-06 14:51:01 +0200

    rebase asem and dennis branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Dennis war hier");
+        System.out.println("Mit seinem 1 side Branch vom Masterbranch");
+
 
 System.out.println("Hello vom asem branch!");
         System.out.println("Hello vom asem branch!");
