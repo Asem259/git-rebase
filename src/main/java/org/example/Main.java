07dac5c6a4c64cef9e2a60a8b5aafe5b1230257a
asem259 <alghabra.asem@gmail.com>
2022-10-06 14:50:48 +0200

    add new branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello vom asem branch!");
         System.out.println("Dennis war hier!");
         System.out.println("Mit seinem 1 side Branch vom Masterbranch!!!");
-
     }
 }
